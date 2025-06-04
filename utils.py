188dcb3ee5e956792782a13d7de2eb96d45a7bfe
HaranHari78 <hvetri7@gmail.com>
2025-06-04 12:26:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,25 @@
-# utils.py
-
-import configparser
-
 import httpx
+import configparser
 from openai import AzureOpenAI
-
 def load_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config
-
-def call_openai_api(prompt: str, model: str):
+def call_openai_api(prompt, model):
     custom_http_client = httpx.Client(verify=False)
-    config = load_config()
+    openai_config = load_config()
+    # AzureOpenAI client using the custom HTTP client
+    openai_client = AzureOpenAI(
+        api_key=openai_config["azure_openai"]["api_key"],
+        api_version=openai_config["azure_openai"]["api_version"],
+        azure_endpoint=openai_config["azure_openai"]["endpoint"],
+        http_client=custom_http_client
+    )
     try:
-        client = AzureOpenAI(
-            api_key=config['azure_openai']['api_key'],
-            api_version=config['azure_openai']['api_version'],
-            azure_endpoint=config['azure_openai']['endpoint'],
-            http_client=custom_http_client
-
-        )
-        response = client.chat.completions.create(
-            model=model,
-            messages=[{"role": "user", "content": prompt}]
-        )
-        return response.choices[0].message.content
+         response = openai_client.chat.completions.create(
+         model=model,messages=[{"role": "user", "content": f"{prompt}"}])
+         print(response.choices[0].message.content)
+         return response.choices[0].message.content
     except Exception as e:
-        print(f"API error: {e}")
-        return ""
+        print("Error during API call:", e)
+        return
